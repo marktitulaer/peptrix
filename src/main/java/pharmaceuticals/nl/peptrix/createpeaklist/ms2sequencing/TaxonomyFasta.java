6d986ed42c4c6a173c1b592e45b0bcd55dbc8743
Mark Titulaer <fm120615@telfort.nl>
2018-01-09 22:43:57 +0100

    genereer uman fasta file 	modified:   src/main/java/pharmaceuticals/nl/peptrix/createpeaklist/ms2sequencing/TaxonomyFasta.java

--- src/main/java/pharmaceuticals/nl/peptrix/createpeaklist/ms2sequencing/TaxonomyFasta.java
+++ src/main/java/pharmaceuticals/nl/peptrix/createpeaklist/ms2sequencing/TaxonomyFasta.java
@@ -1,13 +1,22 @@
 package pharmaceuticals.nl.peptrix.createpeaklist.ms2sequencing;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+import javax.swing.JOptionPane;
+
 import pharmaceuticals.nl.peptrix.Controller;
 import pharmaceuticals.nl.peptrix.export.ExportFileToDisk;
 
 public class TaxonomyFasta {
 
 	Controller cc;
-	
+
 	ExportFileToDisk exportfiletodisk;
+
+	String line;
+
+	String testline;
 
 	public TaxonomyFasta(Controller cc, ExportFileToDisk exportfiletodisk) {
 		this.cc = cc;
@@ -18,14 +27,27 @@ public class TaxonomyFasta {
 
 		String taxonomyFastaFileName = cc.userhome + cc.fileSeparator + string_Taxonomy.replaceAll(" ", "_") + ".fasta";
 
-		boolean taxonomyFastaFileCreated = exportfiletodisk
-				.exportcompletefilename(taxonomyFastaFileName, "".getBytes());
-		
-		System.out.println("taxonomyFastaFileCreated " + taxonomyFastaFileCreated);
-	
-		
-		return taxonomyFastaFileName;
+		boolean taxonomyFastaFileCreated = exportfiletodisk.exportcompletefilename(taxonomyFastaFileName,
+				"".getBytes());
 
+		System.out.println("taxonomyFastaFileCreated " + taxonomyFastaFileCreated);
+
+		try {
+			BufferedReader in = new BufferedReader(new FileReader(found_ms2_database));
+			while ((line = in.readLine()) != null) {
+				System.out.println("twee");
+				testline = line.toLowerCase();
+				System.out.println("  testline " + testline);
+			}
+			System.out.println("file geopend ");
+		} catch (Exception e) {
+			if (cc.debugmode) {
+				e.printStackTrace();
+			} else {
+				JOptionPane.showMessageDialog(null, e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+		return taxonomyFastaFileName;
 	}
 
 }
