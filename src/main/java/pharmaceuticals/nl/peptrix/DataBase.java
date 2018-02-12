c8ab60574a5f699783ecec329acf3a4cc8f90e13
Mark Titulaer <fm120615@telfort.nl>
2018-02-12 13:06:37 +0100

    hibernate adaptions

--- src/main/java/pharmaceuticals/nl/peptrix/DataBase.java
+++ src/main/java/pharmaceuticals/nl/peptrix/DataBase.java
@@ -11,194 +11,248 @@ import pharmaceuticals.nl.peptrix.model.*;
 
 public class DataBase {
 
-	public DataBase() {
-	}
+    public DataBase() {
+    }
 
-	public void FillInitialData() {
+    public void FillInitialData() {
 
-		Session session = null;
-		Transaction transaction = null;
-		try {
-			session = HibernateUtil.getSessionFactory().openSession();
-			transaction = session.getTransaction();
-			transaction.begin();
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.getTransaction();
+            transaction.begin();
 
-			Systemcode systemcode = new Systemcode();
-			Systemcodeitem systemcodeitem = new Systemcodeitem();
-			Unit unit = new Unit();
-			ItemValue itemvalue = new ItemValue();
-			Experiment experiment = new Experiment();
+            Unit unit = new Unit();
+            ItemValue itemvalue = new ItemValue();
+            Experiment experiment = new Experiment();
 
-			Equipment equipment = new Equipment();
-			Query<Equipment> queryequipment = session.createQuery("from Equipment where Code = :code  ");
-			queryequipment.setParameter("code", "MALDITOF");
-			List<Equipment> equipments = queryequipment.list();
-			if (equipments.size() == 0) {
-				equipment.setCode("MALDITOF");
-				equipment.setName("Bruker Daltonics Ultraflex MALDI-TOF");
-				session.save(equipment);
-			}
+            Equipment equipment = new Equipment();
+            Query<Equipment> queryequipment = session.createQuery("from Equipment where Code = :code  ");
+            queryequipment.setParameter("code", "MALDITOF");
+            List<Equipment> equipments = queryequipment.list();
+            if (equipments.size() == 0) {
+                equipment.setCode("MALDITOF");
+                equipment.setName("Bruker Daltonics Ultraflex MALDI-TOF");
+                session.save(equipment);
+            }
 
-			equipment = new Equipment();
-			queryequipment.setParameter("code", "FTICRMS");
-			equipments = queryequipment.list();
-			if (equipments.size() == 0) {
-				equipment.setCode("FTICRMS");
-				equipment.setName("FTMS");
-				session.save(equipment);
-			}
+            equipment = new Equipment();
+            queryequipment.setParameter("code", "FTICRMS");
+            equipments = queryequipment.list();
+            if (equipments.size() == 0) {
+                equipment.setCode("FTICRMS");
+                equipment.setName("FTMS");
+                session.save(equipment);
+            }
 
-			equipment = new Equipment();
-			queryequipment.setParameter("code", "MALDILCMS");
-			equipments = queryequipment.list();
-			if (equipments.size() == 0) {
-				equipment.setCode("MALDILCMS");
-				equipment.setName("MALDI-TOF LC MS");
-				session.save(equipment);
-			}
+            equipment = new Equipment();
+            queryequipment.setParameter("code", "MALDILCMS");
+            equipments = queryequipment.list();
+            if (equipments.size() == 0) {
+                equipment.setCode("MALDILCMS");
+                equipment.setName("MALDI-TOF LC MS");
+                session.save(equipment);
+            }
+
+            equipment = new Equipment();
+            queryequipment.setParameter("code", "ORBITRAP");
+            equipments = queryequipment.list();
+            if (equipments.size() == 0) {
+                equipment.setCode("ORBITRAP");
+                equipment.setName("LTQ Orbitrap LC MS");
+                session.save(equipment);
+            }
+
+            equipment = new Equipment();
+            queryequipment.setParameter("code", "IONTRAP");
+            equipments = queryequipment.list();
+            if (equipments.size() == 0) {
+                equipment.setCode("IONTRAP");
+                equipment.setName("Bruker Ion Trap");
+                session.save(equipment);
+            }
+
+            Group group = new Group();
+            Query<Group> querygroup = session.createQuery("from Group where Group_code = :code  ");
+            querygroup.setParameter("code", "1");
+            List<Group> groups = querygroup.list();
+            if (groups.size() == 0) {
+                group.setGroupcode("1");
+                group.setName("Group 1");
+                session.save(group);
+            }
+
+            Sample sample = new Sample();
+            Query<Sample> querysample = session.createQuery("from Sample where Sample_code = :code  ");
+            querysample.setParameter("code", "1");
+            List<Sample> samples = querysample.list();
+            if (samples.size() == 0) {
+                sample.setSamplecode("1");
+                sample.setName("sample1");
+                session.save(sample);
+            }
+
+            Systemcode systemcode = new Systemcode();
+            Query<Systemcode> querysystemcode = session.createQuery("from Systemcode where Code = :code  ");
+            querysystemcode.setParameter("code", "CALMASSES");
+            List<Systemcode> systemcodes = querysystemcode.list();
+            if (systemcodes.size() == 0) {
+                systemcode.setCode("CALMASSES");
+                systemcode.setDescription("Calibration Masses");
+                session.save(systemcode);
+            }
+
+            systemcode = new Systemcode();
+            querysystemcode.setParameter("code", "MODIFICATIONS");
+            systemcodes = querysystemcode.list();
+            if (systemcodes.size() == 0) {
+                systemcode.setCode("MODIFICATIONS");
+                systemcode.setDescription("Modifications");
+                session.save(systemcode);
+            }
+
+            systemcode = new Systemcode();
+            querysystemcode.setParameter("code", "ENZYME");
+            systemcodes = querysystemcode.list();
+            if (systemcodes.size() == 0) {
+                systemcode.setCode("ENZYME");
+                systemcode.setDescription("Enzyme");
+                session.save(systemcode);
+            }
+
+            Systemcodeitem systemcodeitem = new Systemcodeitem();
+            Query<Systemcodeitem> querysystemcodeitem = session.createQuery("from Systemcodeitem where ItemCode = :code  ");
+            querysystemcodeitem.setParameter("code", "ALBMASSES");
+            List<Systemcodeitem> systemcodeitems =  querysystemcodeitem.list();
+            if (systemcodeitems.size() == 0) {
+                querysystemcode = session.createQuery("from Systemcode where Code = :code  ");
+                querysystemcode.setParameter("code", "CALMASSES");
+                systemcodes = querysystemcode.list();
+                systemcode =  systemcodes.get(0);
+                systemcodeitem.setSystemcode(systemcode);
+                systemcodeitem.setItemcode("ALBMASSES");
+                systemcodeitem.setDescription("Albumine Masses");
+                session.save(systemcodeitem);
+            }
+
+            systemcodeitem = new Systemcodeitem();
+            querysystemcodeitem = session.createQuery("from Systemcodeitem where ItemCode = :code  ");
+            querysystemcodeitem.setParameter("code", "MS2_MOD");
+            systemcodeitems =  querysystemcodeitem.list();
+            if (systemcodeitems.size() == 0) {
+                querysystemcode = session.createQuery("from Systemcode where Code = :code  ");
+                querysystemcode.setParameter("code", "MODIFICATIONS");
+                systemcodes = querysystemcode.list();
+                systemcode =  systemcodes.get(0);
+                systemcodeitem.setSystemcode(systemcode);
+                systemcodeitem.setItemcode("MS2_MOD");
+                systemcodeitem.setDescription("MS2 Modifications");
+                session.save(systemcodeitem);
+            }
+
+            System.out.println("hier 4 ");
 
 
-			equipment = new Equipment();
-			queryequipment.setParameter("code", "ORBITRAP");
-			equipments = queryequipment.list();
-			if (equipments.size() == 0) {
-				equipment.setCode("ORBITRAP");
-				equipment.setName("LTQ Orbitrap LC MS");
-				session.save(equipment);
-			}
+            // group.setName("Group 1");
+            // group.setGroupcode("10");
+            // session.save(group);
 
-			equipment = new Equipment();
-			queryequipment.setParameter("code", "IONTRAP");
-			equipments = queryequipment.list();
-			if (equipments.size() == 0) {
-				equipment.setCode("IONTRAP");
-				equipment.setName("Bruker Ion Trap");
-				session.save(equipment);
-			}
+            // sample.setSamplecode("sample 1");
+            // sample.setName("sample name 1");
+            // session.save(sample);
 
-			Group group = new Group();
-			Query<Group> querygroup = session.createQuery("from Group where Group_code = :code  ");
-			querygroup.setParameter("code", "1");
-			List<Group> groups =  querygroup .list();
-			if (groups.size() == 0) {
-				group.setGroupcode("1");
-				group.setName("Group 1");
-				session.save(group);
-			}
+            // systemcode.setCode("CALMASSES");
+            // systemcode.setDescription("Calibration Masses");
+            // session.save(systemcode);
 
-			Sample sample = new Sample();
-			Query<Sample> querysample = session.createQuery("from Sample where Sample_code = :code  ");
-			querysample.setParameter("code", "1");
-			List<Sample> samples =  querysample.list();
-			if (samples.size() == 0) {
-				sample.setSamplecode("1");
-				sample.setName("sample1");
-				session.save(sample);
-			}
+            // systemcode = new Systemcode();
+            // systemcode.setCode("MODIFICATIONS");
+            // systemcode.setDescription("Modifications");
+            // session.save(systemcode);
 
+            // systemcode = new Systemcode();
+            // systemcode.setCode("ENZYME");
+            // systemcode.setDescription("Enzyme");
+            // session.save(systemcode);
 
-			System.out.println("hier ");
+            // systemcodeitem.setItemcode("ALBMASSES");
+            // systemcodeitem.setDescription("Albumine Masses");
+            // session.save(systemcodeitem);
 
+            // systemcodeitem = new Systemcodeitem();
+            // systemcodeitem.setItemcode("MS2_MOD");
+            // systemcodeitem.setDescription("MS2 Modifications");
+            // session.save(systemcodeitem);
 
-			// group.setName("Group 1");
-			// group.setGroupcode("10");
-			// session.save(group);
+            // systemcodeitem = new Systemcodeitem();
+            // systemcodeitem.setItemcode("ENZYME");
+            // systemcodeitem.setDescription("Enzyme");
+            // session.save(systemcodeitem);
 
-			// sample.setSamplecode("sample 1");
-			// sample.setName("sample name 1");
-			// session.save(sample);
+            // unit.setType("mass");
+            // unit.setUnitvalue("m/z");
+            // session.save(unit);
 
-			// systemcode.setCode("CALMASSES");
-			// systemcode.setDescription("Calibration Masses");
-			// session.save(systemcode);
+            // itemvalue.setItemvalue("960.5631");
+            // session.save(itemvalue);
 
-			// systemcode = new Systemcode();
-			// systemcode.setCode("MODIFICATIONS");
-			// systemcode.setDescription("Modifications");
-			// session.save(systemcode);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("1000.6043");
+            // session.save(itemvalue);
 
-			// systemcode = new Systemcode();
-			// systemcode.setCode("ENZYME");
-			// systemcode.setDescription("Enzyme");
-			// session.save(systemcode);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("1149.6156");
+            // session.save(itemvalue);
 
-			// systemcodeitem.setItemcode("ALBMASSES");
-			// systemcodeitem.setDescription("Albumine Masses");
-			// session.save(systemcodeitem);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("1511.8433");
+            // session.save(itemvalue);
 
-			// systemcodeitem = new Systemcodeitem();
-			// systemcodeitem.setItemcode("MS2_MOD");
-			// systemcodeitem.setDescription("MS2 Modifications");
-			// session.save(systemcodeitem);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("2045.0959");
+            // session.save(itemvalue);
 
-			// systemcodeitem = new Systemcodeitem();
-			// systemcodeitem.setItemcode("ENZYME");
-			// systemcodeitem.setDescription("Enzyme");
-			// session.save(systemcodeitem);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Carbamidomethyl (C)");
+            // session.save(itemvalue);
 
-			// unit.setType("mass");
-			// unit.setUnitvalue("m/z");
-			// session.save(unit);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Oxidation (M)");
+            // session.save(itemvalue);
 
-			// itemvalue.setItemvalue("960.5631");
-			// session.save(itemvalue);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Phosphorylation (S)");
+            // session.save(itemvalue);
 
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("1000.6043");
-			// session.save(itemvalue);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Phosphorylation (T)");
+            // session.save(itemvalue);
 
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("1149.6156");
-			// session.save(itemvalue);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Phosphorylation (Y)");
+            // session.save(itemvalue);
 
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("1511.8433");
-			// session.save(itemvalue);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Trypsin");
+            // session.save(itemvalue);
 
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("2045.0959");
-			// session.save(itemvalue);
+            // itemvalue = new ItemValue();
+            // itemvalue.setItemvalue("Chymotrypsin");
+            // session.save(itemvalue);
 
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Carbamidomethyl (C)");
-			// session.save(itemvalue);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
 
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Oxidation (M)");
-			// session.save(itemvalue);
-
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Phosphorylation (S)");
-			// session.save(itemvalue);
-
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Phosphorylation (T)");
-			// session.save(itemvalue);
-
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Phosphorylation (Y)");
-			// session.save(itemvalue);
-
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Trypsin");
-			// session.save(itemvalue);
-
-			// itemvalue = new ItemValue();
-			// itemvalue.setItemvalue("Chymotrypsin");
-			// session.save(itemvalue);
-
-			transaction.commit();
-		} catch (Exception e) {
-			if (transaction != null) {
-				transaction.rollback();
-			}
-		} finally {
-			if (session != null) {
-				session.close();
-			}
-		}
-
-		HibernateUtil.shutdown();
-	}
+        HibernateUtil.shutdown();
+    }
 
 }
